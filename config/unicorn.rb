6ae5e19ec57fd9dd49dc12d0be04b11c62de27dd
keisuke-kk <candy0220.k@gmail.com>
2020-04-24 20:39:38 +0900

    aaaa

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -6,7 +6,7 @@ worker_processes 1
 working_directory "#{app_path}/current"
 
 #アプリケーションの設置されているディレクトリを指定
-working_directory app_path
+# working_directory app_path
 
 #ポート番号を指定
 listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
